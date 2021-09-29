7aedd44f1a2d4f57d84bd6469889349b9c7ce40a
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-29 09:14:42 +0800

    Add project files.

---
+++ Wednesday_Personal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wednesday_Personal
+{
+
+    public delegate void HelloFunctionDelegate(string Message);
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //// A delegate is a type safe function pointer
+            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+            del("Hello from Delegate");
+        }
+
+
+
+
+        static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+            Console.ReadLine();
+        }
+
+    }
+
+   
+}
